9913e7644880879b7d9dfd5bfe2c2f2878128daa
mcaci <michele.caci@gmail.com>
2021-03-30 19:34:58 +0200

    Add MoveOne func to set

--- set/move_cards.go
+++ set/move_cards.go
@@ -1,7 +1,24 @@
 package set
 
-// Move moves cards from first to second set in parameters
+import (
+	"fmt"
+
+	"github.com/mcaci/ita-cards/card"
+)
+
+// Move moves all cards from first to second set in parameters
 func Move(from, to *Cards) {
 	to.Add(*from...)
 	from.Clear()
+}
+
+// MoveOne moves one card from first to second set in parameters
+func MoveOne(c *card.Item, from, to *Cards) error {
+	i := from.Find(*c)
+	if i < 0 {
+		return fmt.Errorf("card (%v) not found in the source set (%v)", *c, *from)
+	}
+	to.Add((*from)[i])
+	*from = append((*from)[:i], (*from)[i+1:]...)
+	return nil
 }
--- set/move_cards_test.go
+++ set/move_cards_test.go
@@ -2,6 +2,8 @@ package set
 
 import (
 	"testing"
+
+	"github.com/mcaci/ita-cards/card"
 )
 
 func TestCardsAreMoved(t *testing.T) {
@@ -9,7 +11,7 @@ func TestCardsAreMoved(t *testing.T) {
 	to := NewMust()
 	Move(from, to)
 	if len(*from) > 0 {
-		t.Fatalf("From should be empty but has %v", from)
+		t.Fatalf("from should be empty but has %v", from)
 	}
 }
 
@@ -18,6 +20,27 @@ func TestPlayerCountPoints(t *testing.T) {
 	to := NewMust()
 	Move(from, to)
 	if len(*to) == 0 {
-		t.Fatalf("To should be empty but has %v", to)
+		t.Fatal("to should not be empty but is")
+	}
+}
+
+func TestMoveOneCard(t *testing.T) {
+	from := NewMust(1, 2, 3, 4, 5)
+	to := NewMust()
+	MoveOne(card.MustID(2), from, to)
+	if len(*to) == 0 {
+		t.Fatal("to should not be empty but is")
+	}
+	if id := (*to)[0].ToID(); id != 2 {
+		t.Fatalf("moved card should be the one with ID == 2 but was %d", id)
+	}
+}
+
+func TestMoveOneCardError(t *testing.T) {
+	from := NewMust(1, 2, 3, 4, 5)
+	to := NewMust()
+	err := MoveOne(card.MustID(21), from, to)
+	if err == nil {
+		t.Fatal("expecting a not found error here but no errors were raised")
 	}
 }
